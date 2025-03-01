4e687b2d09b44ce50d33725ad9370a01734a46b7
weirdmrn123 <weirdisrael41@gmail.com>
2025-03-01 20:45:04 +0100

    Initial commit

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ app = FastAPI()
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Change this in production
+    allow_origins=[""],  # Change this in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
